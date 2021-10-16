32ae33815f25ccc9048e674ef74d40133f2d9225
tasnimul2 <chowdhury1478@gmail.com>
2021-10-15 23:50:26 -0400

    api can now call zipcodes and display info, still need to do city search and design the app

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,66 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+  <div>
+    <p>City : {props.info.City}</p>
+    <p>State : {props.info.State}</p>
+    <p>Location : ({props.info.Lat} , {props.info.Long})</p>
+    <p>Population : {props.info.EstimatedPopulation}</p>
+    <p>Total Wages : {props.info.TotalWages}</p>
+    <p>---------------------------------------</p>
+  </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div>
+      <input onChange={ (e)=> props.handleZipUpdate(e)} />
+    </div>);
 }
 
 
 class App extends Component {
+
+  state = {
+    zipcode : '',
+    cities : []
+  }
+
+  updateZipcode = (event)=>{
+
+    console.log(event.target.value)
+    this.setState({
+      zipcode : event.target.value,
+    })
+
+    if(event.target.value.length === 5){
+      console.log(`the zipcode is ${this.state.zipcode}`)
+
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+
+        this.setState({
+          cities : data
+        })
+      })
+      .catch(error => {
+        console.log("error retrieving data");
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField handleZipUpdate={ (event) => this.updateZipcode(event)} />
         <div>
-          <City />
-          <City />
+          { this.state.cities.map( city => <City info={city} />)}
         </div>
       </div>
     );
